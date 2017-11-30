25fd0a8396613c941b80b8d3de40e12548069fdd
Jean Oliveira <oli15014@byui.edu>
2017-11-29 20:55:46 -0700

    Perform sorting, filtering, paging, and grouping.

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -1,28 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+            public async Task<ActionResult> About()
+            {
+                IQueryable<EnrollmentDateGroup> data =
+                    from student in _context.Students
+                    group student by student.EnrollmentDate into dateGroup
+                    select new EnrollmentDateGroup()
+                    {
+                        EnrollmentDate = dateGroup.Key,
+                        StudentCount = dateGroup.Count()
+                    };
+                return View(await data.AsNoTracking().ToListAsync());
+            }
 
-            return View();
-        }
-
-        public IActionResult Contact()
+            public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
 

0bf365b5e2ec6960f1465e9836acd2374c304f4e
Jean Oliveira <oli15014@byui.edu>
2017-12-02 19:47:03 -0700

    Migrations. Creating a complex data model.

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -45,6 +45,6 @@ namespace ContosoUniversity.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        } 
     }
 }
